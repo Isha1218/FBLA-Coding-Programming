d898ccaed127e318c21fc6f7781104486125ea01
Isha1218 <52381965+Isha1218@users.noreply.github.com>
2022-04-14 09:33:31 -0700

    Update Attribute4.swift

--- Attribute4.swift
+++ Attribute4.swift
@@ -4,11 +4,14 @@
 //
 //  Created by Ishita Mundra on 12/31/21.
 //
-
 import SwiftUI
 
-struct Attribute4View: View {
+// This view allows the user to input the fourth attribute
+// The fourth attribute is the season when the user wishes to go to the tourist location
+struct Attribute4: View {
     
+    // attribute1, attribute2, and attribute3 are passed into this view
+    // attribute4 will be assigned a value in this view and passed along to the other views
     @Binding var attribute1: String
     @Binding var attribute2: String
     @Binding var attribute3: String
@@ -16,6 +19,8 @@ struct Attribute4View: View {
     @State var isActive = false
     
     var body: some View {
+        
+        // Layout and design of the page
         ZStack {
             Image("Tulip Field")
                 .resizable()
@@ -33,6 +38,9 @@ struct Attribute4View: View {
                     .padding()
                     .shadow(color: Color.black, radius: 1, x:1, y:1)
                 Spacer()
+                
+                // Similar dropdown menus for the other attribute pages, but with different items listed
+                // Contains the 4 seasons: Winter, Spring, Summer, Fall
                 GroupBox {
                     DisclosureGroup("Season") {
                         Button("Fall", action: {
@@ -59,11 +67,13 @@ struct Attribute4View: View {
                             .padding(.top)
                     }
                     .frame(width: 300)
-                    .foregroundColor(Color.accentColor)
                 }
                 Spacer()
+                
+                // After the user has clicked one of the buttons from the dropdown menu, the Attribute5 page shows up
+                // The variables, attribute1, attribute2, attribute 3, and attribute4 are carried over to the next view
                 .background(
-                    NavigationLink(destination: Attribute5View(attribute1: $attribute1, attribute2: $attribute2, attribute3:$attribute3, attribute4: $attribute4), isActive: $isActive) {
+                    NavigationLink(destination: Attribute5(attribute1: $attribute1, attribute2: $attribute2, attribute3:$attribute3, attribute4: $attribute4), isActive: $isActive) {
                         EmptyView()
                                 
                 })
@@ -73,8 +83,8 @@ struct Attribute4View: View {
     
 }
 
-struct Attribute4View_Previews: PreviewProvider {
+struct Attribute4_Previews: PreviewProvider {
     static var previews: some View {
-        Attribute4View(attribute1: .constant(""), attribute2: .constant(""), attribute3: .constant(""))
+        Attribute4(attribute1: .constant(""), attribute2: .constant(""), attribute3: .constant(""))
     }
 }
