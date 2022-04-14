874d51f389a13206cb139fd58046db3f74419995
Isha1218 <52381965+Isha1218@users.noreply.github.com>
2022-04-14 09:32:53 -0700

    Update Attribute2.swift

--- Attribute2.swift
+++ Attribute2.swift
@@ -4,16 +4,21 @@
 //
 //  Created by Ishita Mundra on 12/29/21.
 //
-
 import SwiftUI
 
-struct Attribute2View: View {
+// This view allows the user to input the second attribute
+// The second attribute is the type of location (e.g. theme park, nature, etc.)
+struct Attribute2: View {
     
+    // attribute1 is a Binding to indicate that it was declared in another view
+    // attribute2 will be asssigned a value in this view and passed along to the other views
     @Binding var attribute1: String
     @State var attribute2 = ""
     @State var isActive = false
     
     var body: some View {
+        
+        // Layout and design of the page
         ZStack {
             Image("The Seattle Great Wheel")
                 .resizable()
@@ -31,6 +36,9 @@ struct Attribute2View: View {
                     .padding()
                     .shadow(color: Color.black, radius: 1, x:1, y:1)
                 Spacer()
+                
+                // Similar dropdown menu for the Attribute1 page. but with different items listed
+                // Contains the types of locations: Historical, Themepark, Nature, Landmark, Culture, Art, and Ski
                 GroupBox {
                     DisclosureGroup("Type of Attraction") {
                         Button("Historical Attraction", action: {
@@ -68,29 +76,32 @@ struct Attribute2View: View {
                         })
                             .padding(.top)
                         
-                        Button("Other", action: {
-                            attribute2 = "Other"
+                        Button("Ski", action: {
+                            attribute2 = "Ski"
                             isActive = true
                         })
                             .padding(.top)
                     }
                     .frame(width: 300)
-                    .foregroundColor(Color.accentColor)
                 }
                 Spacer()
+                
+                // After the user has clicked one of the buttons from the dropdown menu, the Attribute3 page shows up
+                // The variables, attribute1 and attribute2 are carried over to the next view
                 .background(
-                    NavigationLink(destination: Attribute3View(attribute1: $attribute1, attribute2: $attribute2), isActive: $isActive) {
+                    NavigationLink(destination: Attribute3(attribute1: $attribute1, attribute2: $attribute2), isActive: $isActive) {
                         EmptyView()
-                            
                 })
             }
         }
+
     }
 }
 
-struct Attribute2View_Previews: PreviewProvider {
+
+
+struct Attribute2_Previews: PreviewProvider {
     static var previews: some View {
-        Attribute2View(attribute1: .constant(""))
+        Attribute2(attribute1: .constant(""))
     }
 }
-
