493eb13608dae3d2051d43a4e9da751fb8224029
Isha1218 <52381965+Isha1218@users.noreply.github.com>
2022-04-14 09:33:51 -0700

    Update Attribute5.swift

--- Attribute5.swift
+++ Attribute5.swift
@@ -4,11 +4,14 @@
 //
 //  Created by Ishita Mundra on 12/31/21.
 //
-
 import SwiftUI
 
-struct Attribute5View: View {
+// This view allows the user to input the fifth attribute
+// The fifth attribute is the age group of the individuals that will be going to the tourist location
+struct Attribute5: View {
     
+    // attribute1, attribute2, attribute3, and attribute4 are passed into this view
+    // attribute5 will be assigned a value in this view and passed along SuggestAttractions
     @Binding var attribute1: String
     @Binding var attribute2: String
     @Binding var attribute3: String
@@ -17,6 +20,8 @@ struct Attribute5View: View {
     @State var isActive = false
     
     var body: some View {
+        
+        // Layout and design of page
         ZStack {
             Image("Stevens Pass")
                 .resizable()
@@ -34,6 +39,9 @@ struct Attribute5View: View {
                     .padding()
                     .shadow(color: Color.black, radius: 1, x:1, y:1)
                 Spacer()
+                
+                // Similar dropdown menus for the other attribute pages, but with different items listed
+                // Contains the 3 main age groups: Kids, Adults, and Seniors
                 GroupBox {
                     DisclosureGroup("Required") {
                         
@@ -56,11 +64,13 @@ struct Attribute5View: View {
                             .padding(.top)
                     }
                     .frame(width: 300)
-                    .foregroundColor(Color.accentColor)
                 }
                 Spacer()
+                
+                // After the user has clicked one of the buttons from the dropdown menu, the program will suggest the top five locations based on the five attributes that the user inputtted
+                // All five attribute variables are carried over to the SuggestAttractions view and added to a userAttributes array
                 .background(
-                    NavigationLink(destination: SuggestAttractionsView(attribute1: $attribute1, attribute2: $attribute2, attribute3: $attribute3, attribute4: $attribute4, attribute5: $attribute5), isActive:$isActive) {
+                    NavigationLink(destination: SuggestAttractions(attribute1: $attribute1, attribute2: $attribute2, attribute3: $attribute3, attribute4: $attribute4, attribute5: $attribute5), isActive:$isActive) {
                         EmptyView()
                     }
                 )
@@ -70,8 +80,8 @@ struct Attribute5View: View {
     
 }
 
-struct Attribute5View_Previews: PreviewProvider {
+struct Attribute5_Previews: PreviewProvider {
     static var previews: some View {
-        Attribute5View(attribute1: .constant(""), attribute2: .constant(""), attribute3: .constant(""), attribute4: .constant(""))
+        Attribute5(attribute1: .constant(""), attribute2: .constant(""), attribute3: .constant(""), attribute4: .constant(""))
     }
 }
