b9f28047453e1d59e5d863f559bd8106dc5dfe6b
Isha1218 <52381965+Isha1218@users.noreply.github.com>
2022-04-14 09:35:59 -0700

    Update AttractionsDetailView.swift

--- AttractionsDetailView.swift
+++ AttractionsDetailView.swift
@@ -4,20 +4,27 @@
 //
 //  Created by Ishita Mundra on 3/19/22.
 //
-
 import SwiftUI
 import MapKit
 
+// This view gives a more detailed overview for the selected attraction that the user wants to learn more about
 struct AttractionsDetailView: View {
+    
+    // Takes in the attraction passed in by the SuggestAttractions view
     var attraction: Attraction
     
     var body: some View {
+        
+        // Passes the coordinates defined for each attraction object and the attraction's name to the MapView
+        // A map is then displayed in this view
         MapView(coordinate: CLLocationCoordinate2D(latitude: attraction.coordinate.latitude, longitude: attraction.coordinate.longitude), name: attraction.name)
         Text(attraction.name)
             .foregroundColor(.primary)
             .font(.title)
             .padding()
         VStack {
+            
+            // A list of information of the attributes for the specific location is provided
             List {
                 HStack {
                     Text("More information")
@@ -51,4 +58,3 @@ struct AttractionsDetailView_Previews: PreviewProvider {
         AttractionsDetailView(attraction: Attraction(name: "", loc: "", typeOfLoc: "", price: "", season: "", age: "", coordinate: CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)))
     }
 }
- 
