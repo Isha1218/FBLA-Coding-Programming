aef32603554cfe2f24de63194d98a546b7b343fa
Isha1218 <52381965+Isha1218@users.noreply.github.com>
2022-04-14 09:34:39 -0700

    Add files via upload

---
+++ FBLA_WAtourApp.swift
@@ -0,0 +1,17 @@
+//
+//  FBLA_WAtourApp.swift
+//  FBLA WAtour
+//
+//  Created by Ishita Mundra on 4/9/22.
+//
+
+import SwiftUI
+
+@main
+struct FBLA_WAtourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
